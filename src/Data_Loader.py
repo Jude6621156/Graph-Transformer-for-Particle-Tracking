0e32d9e3984a49b7f1198711a60c81eb90c9194c
Jude6621156 <judesioufi@gmail.com>
2026-02-13 13:33:30 +0000

    Added file for getting data

---
+++ src/Data_Loader.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+
+def load_event(eventId, data_path):
+    fHits = os.path.join(data_path, f"{eventId}-hits.csv")
+    fTruth = os.path.join(data_path, f"{eventId}-truth.csv")
+    fParticles = os.path.join(data_path, f"{eventId}-particles.csv")
+
+    hits = pd.read_csv(fHits)
+    truth = pd.read_csv(fTruth)
+    particles = pd.read_csv(fParticles)
+
+    hits = hits.merge(truth[["hit_id", "particle_id"]], on = "hit_id", how = "left")
+
+    return hits, particles
